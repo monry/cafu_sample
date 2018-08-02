ded9adbff2eb396cf3d87c7ed6f5eb704a78f067
Tetsuya Mori <monry84@gmail.com>
2018-08-02 18:59:00 +0900

    :white_check_mark: Write tests

---
+++ Assets/Tests/PlayMode/Scripts/Domain/UseCase/GameStateUseCaseTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using Monry.CAFUSample.Domain.Model;
+using Moq;
+using NUnit.Framework;
+using UniRx;
+using UnityEngine.TestTools;
+using Zenject;
+
+namespace Monry.CAFUSample.Domain.UseCase
+{
+    public class GameStateUseCaseTest : ZenjectUnitTestFixture
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Container.Bind<IGameStateModel>().FromMock();
+            Container.Bind<IGameScoreRenderablePresenter>().FromMock();
+            Container.Bind<GameStateUseCase>().AsTransient();
+        }
+
+        [UnityTest]
+        public IEnumerator ResetScoreTest()
+        {
+            var rp = new IntReactiveProperty();
+            var mock = new Mock<IGameStateModel>();
+            mock.Setup(x => x.Score).Returns(rp);
+
+            Container.Rebind<IGameStateModel>().FromInstance(mock.Object);
+
+            var useCase = Container.Resolve<GameStateUseCase>();
+
+            {
+                var subscription = rp.First().ToYieldInstruction();
+                yield return subscription;
+                Assert.AreEqual(0, subscription.Result);
+            }
+
+            // Pass しない…
+            {
+                var subscription = rp.First().ToYieldInstruction();
+                rp.Value = 10;
+                yield return subscription;
+                Assert.AreEqual(10, subscription.Result);
+            }
+
+            {
+                var subscription = rp.First().ToYieldInstruction();
+                useCase.ResetScore();
+                yield return subscription;
+                Assert.AreEqual(0, subscription.Result);
+            }
+        }
+    }
+}
