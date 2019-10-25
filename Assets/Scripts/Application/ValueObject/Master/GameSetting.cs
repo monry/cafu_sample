ba4f271138087783c7e9a58e0b82cfc7e721b483
Tetsuya Mori <monry84@gmail.com>
2019-10-25 16:45:00 +0900

    :sparkles: WIP: Add signal and setting

---
+++ Assets/Scripts/Application/ValueObject/Master/GameSetting.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace CAFUSample.Application.ValueObject.Master
+{
+    [CreateAssetMenu(menuName = "ValueObject/CAFUSample/Master/GameSetting", fileName = "GameSetting")]
+    public class GameSetting : ScriptableObject
+    {
+
+    }
+}
